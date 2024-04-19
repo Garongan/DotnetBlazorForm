92663188665e3dc11993261a7969b6eadcbbfc96
Alvindo Tri Jatmiko <alvindo56@gmail.com>
2024-04-20 04:23:59 +0700

    feat: auto complete for details stuff and price stuff

--- wwwroot/js/home.js
+++ wwwroot/js/home.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     let kd_negara = "";
+    let hs_code = "";
 
     // auto complete country name
     $("#CountryName").autocomplete({
@@ -27,8 +28,8 @@ $(document).ready(function () {
                 },
             });
         },
-        select: function (event, ui) {
-            kd_negara = ui.item.kd_negara;
+        select: function (event, data) {
+            kd_negara = data.item.kd_negara;
         },
     });
 
@@ -57,5 +58,48 @@ $(document).ready(function () {
                 },
             });
         },
+    });
+
+    // auto complete details stuff from code code
+    $("#StuffCode").on("keyup", function () {
+        hs_code = $(this).val();
+        $.ajax({
+            url: "http://localhost:3000/barang?hs_code=" + hs_code,
+            method: "GET",
+            dataType: "json",
+            success: function (data) {
+                if (data) {
+                    $("#StuffDetails").val(data[0].details);
+                }
+            },
+        });
+    });
+
+    // auto complete stuff price
+    $("#StuffPrice").on("keyup", function () {
+        const toCurrency = (price) => {
+            return price
+                .toLocaleString("id-ID", {
+                    style: "currency",
+                    currency: "IDR",
+                })
+                .replace(/Rp|,00/g, "")
+                .trim();
+        };
+        $("#StuffPrice").val(toCurrency($(this).val()));
+        const price = toCurrency($(this).val());
+        $.ajax({
+            url: "http://localhost:3000/tarif?hs_code=" + hs_code,
+            method: "GET",
+            dataType: "json",
+            success: function (data) {
+                if (data) {
+                    const dutyPrice = data.rate_bea_masuk;
+
+                    $("#StuffDuty").val(dutyPrice);
+                    $("#StuffDutyTotal").val(toCurrency(price * (dutyPrice / 100)));
+                }
+            },
+        });
     });
 });
